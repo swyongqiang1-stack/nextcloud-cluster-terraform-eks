8c08aaa0026aee155807b3bb27b2c203e32b1b83
Elden <swyongqiang1@gmail.com>
2026-08-27 21:23:43 +0800

    修改 ingress

--- terraform/eks/helm/ingress-nginx.tf
+++ terraform/eks/helm/ingress-nginx.tf
@@ -5,39 +5,15 @@ resource "helm_release" "ingress_nginx" {
   namespace  = "ingress-nginx"
   create_namespace = true
 
-  set {
-      name  = "controller.service.type"
-      value = "LoadBalancer"
-    }
-
-  set {
-      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
-      value = "external"
-  }
-  set {
-      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-nlb-target-type"
-      value = "ip"
-  }
-  
-  set {    
-      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
-      value = "internet-facing"
-}
-  set{
-      name  = "controller.metrics.enabled"
-      value = "true"
-  }
-  set{
-      name  = "controller.metrics.serviceMonitor.enabled"
-      value = "true"
-  }
-}
+  values = [
+    file("${path.module}/values/ingress_nginx.yaml")
+  ]
 
 resource "kubernetes_ingress_v1" "nextcloud" {
   wait_for_load_balancer = true
   metadata {
     name = "nextcloud"
-    namespace = "dev"
+    namespace = "nextcloud"
   }
   spec {
     ingress_class_name = "nginx"
